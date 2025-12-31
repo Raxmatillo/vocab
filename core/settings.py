0ece6f1db740973b27e08cda86887194f43f31d2
Raxmatillo <milodcomposer@gmail.com>
2025-12-31 18:15:24 +0500

    update cors

--- core/settings.py
+++ core/settings.py
@@ -156,6 +156,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "https://vocabuz.netlify.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
